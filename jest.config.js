18aaa49dc875103841e09490c6e72cf82cabcec6
Adib Grouz <contact@adib-grouz.com>
2024-09-01 22:22:59 +0200

    update deps

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,12 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/tests/**/*.test.ts'],
-    moduleFileExtensions: ['ts', 'js'],
-    transform: {
-      '^.+\\.ts$': 'ts-jest',
-    },
-    collectCoverage: true,
-    coverageDirectory: 'coverage',
-    coverageReporters: ['text', 'lcov'],
-  };
-  
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/tests/**/*.test.ts"],
+  moduleFileExtensions: ["ts", "js"],
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+  },
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov"],
+};
